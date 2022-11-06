4f10dbc45de125c9890df093f401e4dbde9ac7f9
Robert Uridge <856071+ruridge@users.noreply.github.com>
2022-11-06 15:21:31 +0800

    Less oppionated prettier config

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import {StatusBar, useColorScheme} from 'react-native';
-import {AppNavigator} from './src/navigators/app-navigator';
+import { StatusBar, useColorScheme } from 'react-native';
+import { AppNavigator } from './src/navigators/app-navigator';
 
 export default function App() {
   const isDarkMode = useColorScheme() === 'dark';
